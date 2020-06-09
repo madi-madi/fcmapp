d071f6071b371349bd725e870307deba5e4c5f8e
madi-madi <ibrahim.s.m.2010@gmail.com>
2020-06-09 11:59:21 +0300

    create file fcmapp/public/firebasse.js and add config firebase

---
+++ public/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+                    apiKey: "hidden it",
+                    authDomain: "hidden it",
+                    databaseURL: "hidden it",
+                    projectId: "hidden it",
+                    storageBucket: "hidden it",
+                    messagingSenderId: "hidden it",
+                    appId: "hidden it",
+                    measurementId: "hidden it"
+                  };
