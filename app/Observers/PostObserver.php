0d94ead2d1320bf42a293dcc624337bc4e64b691
madi-madi <ibrahim.s.m.2010@gmail.com>
2020-06-15 23:43:55 +0300

    post observers

---
+++ app/Observers/PostObserver.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Observers;
+
+use App\Post;
+use FCMAPP;
+use DB;
+class PostObserver
+{
+    /**
+     * Handle the post "created" event.
+     *
+     * @param  \App\Post  $post
+     * @return void
+     */
+    public function created(Post $post)
+    {
+        $title ="Fcmapp";
+        $body = "New Post : ".$post->title;
+        $icon =null;
+        $data = $post;
+        $device_token = DB::table('users')->where('id',2)->where('fcm_token','!=','')->pluck('fcm_token')->toArray();
+        $ob = new FCMAPP;
+         $result = $ob->sendTo($device_token,$title,$body,$icon,$data);
+    }
+
+    /**
+     * Handle the post "updated" event.
+     *
+     * @param  \App\Post  $post
+     * @return void
+     */
+    public function updated(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Handle the post "deleted" event.
+     *
+     * @param  \App\Post  $post
+     * @return void
+     */
+    public function deleted(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Handle the post "restored" event.
+     *
+     * @param  \App\Post  $post
+     * @return void
+     */
+    public function restored(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Handle the post "force deleted" event.
+     *
+     * @param  \App\Post  $post
+     * @return void
+     */
+    public function forceDeleted(Post $post)
+    {
+        //
+    }
+}
