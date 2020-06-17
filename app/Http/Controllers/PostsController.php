5cd191293dd87b50eca83587e04f4bf7dc3c8ff5
madi-madi <ibrahim.s.m.2010@gmail.com>
2020-06-17 18:41:38 +0300

    add post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Facades\Validator;
 
 class PostsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
 
     public static $post = Null;
 
@@ -28,11 +23,6 @@ class PostsController extends Controller
         return view('posts.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('posts.create');
@@ -41,7 +31,6 @@ class PostsController extends Controller
 
     public function store(Request $request)
     {
-      //  dd($request->all());
       $validator = Validator::make($request->all(), [ // <---
         'title' => 'required|unique:posts|max:255',
         'body' => 'required',
@@ -60,46 +49,21 @@ class PostsController extends Controller
         ->withInput(['status'=>"dome add post"]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
