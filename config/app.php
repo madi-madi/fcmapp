a07eef3cb14965ece7847f136d4e757224e5f41b
madi-madi <ibrahim.s.m.2010@gmail.com>
2020-06-17 18:44:22 +0300

    add fcm on aliases

--- config/app.php
+++ config/app.php
@@ -226,6 +226,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'FCMAPP' => App\Http\Controllers\FcmController::class,
+
 
     ],
 
